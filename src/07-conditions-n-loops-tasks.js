63a927c24398ce58783dce772267adb79734f924
Aliaksandr Fedukovich <1632637@mail.ru>
2023-05-29 16:12:45 +0300

    feat: add solution

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -391,8 +391,15 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  const arr = [];
+  let temp = num;
+  while (temp) {
+    arr.push(temp % n);
+    temp = Math.floor(temp / n);
+  }
+  return arr.reverse().join('');
+/*   throw new Error('Not implemented'); */
 }
 
 

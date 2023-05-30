16ab5fb947b9c948717fa5711eec492e55691196
Aliaksandr Fedukovich <1632637@mail.ru>
2023-05-30 08:34:00 +0300

    feat: add solution

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -59,8 +59,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
+/*   throw new Error('Not implemented'); */
 }
 
 

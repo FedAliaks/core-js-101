1cc224d90e61faa8644aa0557277c8f6382d08c3
Aliaksandr Fedukovich <1632637@mail.ru>
2023-06-16 14:45:37 +0300

    feat: add solution

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -80,8 +80,9 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
+  /* throw new Error('Not implemented'); */
 }
 
 /**
